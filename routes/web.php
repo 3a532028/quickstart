364c83fdb6a26f0a6cb8bbcbc06b710a69165819
Bruce <s3a532028@student.ncut.edu.tw>
2018-10-24 01:24:15 +0800

    增加任務

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,21 @@ Route::get('/', function () {
 
 
 Route::post('/task', function (Request$request) {
-   //
+    // 驗證輸入
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    // 建立該任務...
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
+
 
 });
 
